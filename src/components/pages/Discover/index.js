037d0cb51e9d0eb8e1dbd062102837904e93f129
Shea Mullaney <seagda@gmail.com>
2021-02-16 14:27:02 -0800

    adding cards

--- src/components/pages/Discover/index.js
+++ src/components/pages/Discover/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import API from '../../../utils/API';
+import Card from '../../../components/Card';
+import Alert from '../../../components/Alert';
 
 export default class Discover extends Component {
     state = { image: "", friends: 0 };
@@ -26,13 +28,13 @@ export default class Discover extends Component {
                     <h1>Make New Friends</h1>
                 </div>
                 <div className="row justify-content-center">
-                    <h3>Thumbs up on any pups you'd like to meet!</h3>
+                    <h3>Thumbs up on any pups you'd like to meet!</h3>0
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-sm-12 col-m-8 col-l-6 col-xl-4">
-                        <div className="card">
-                            <img src={this.state.image} />
-                        </div>
+                    <h1>Made friends with {this.state.matchCount} pups so far!</h1>
+                    <Card image={this.state.image} handleThumbsUp={this.handleThumbsUp} />
+                    <Alert style={{ opacity: this.state.match ? 1 : 0 }} type="success">Hooray! They Like You, Too!</Alert>
                     </div>
                 </div>
             </div>
