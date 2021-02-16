684ba6caf374b156f8cd7aaaa7effa7bba4eba09
JohnDJake <john.d.jake@gmail.com>
2021-02-16 13:47:24 -0800

    actually get random image now

--- src/components/pages/Discover/index.js
+++ src/components/pages/Discover/index.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import API from '../../../utils/API';
 
 export default class Discover extends Component {
+    state = { image: "", friends: 0 };
+
+    componentDidMount() {
+        API.random().then(({ data: { message } }) => this.setState({ image: message }));
+    }
+
     render() {
         return (
-            <div>
-                <h1 className="text-center">Make New Friends</h1>
-                <h3 className="text-center">Thumbs up on any pups you'd like to meet!</h3>
-                <div className="card">
-                    <img src={API.random()} className="card-img" />
+            <div className="container">
+                <div className="row justify-content-center">
+                    <h1>Make New Friends</h1>
+                </div>
+                <div className="row justify-content-center">
+                    <h3>Thumbs up on any pups you'd like to meet!</h3>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-sm-12 col-m-8 col-l-6 col-xl-4">
+                        <div className="card">
+                            <img src={this.state.image} />
+                        </div>
+                    </div>
                 </div>
             </div>
         )
