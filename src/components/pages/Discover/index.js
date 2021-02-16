0a35a3a18d4e0953110f0fc56ae008b6384f6184
Shea Mullaney <seagda@gmail.com>
2021-02-16 15:41:10 -0800

    buttons kinda work

--- src/components/pages/Discover/index.js
+++ src/components/pages/Discover/index.js
@@ -10,7 +10,7 @@ export default class Discover extends Component {
         this.getNewPup();
     }
 
-    getNewPup() {
+    getNewPup = () => {
         API.random().then(({ data: { message } }) => this.setState({ image: message }));
     }
 
@@ -28,12 +28,12 @@ export default class Discover extends Component {
                     <h1>Make New Friends</h1>
                 </div>
                 <div className="row justify-content-center">
-                    <h3>Thumbs up on any pups you'd like to meet!</h3>0
+                    <h3>Thumbs up on any pups you'd like to meet!</h3>
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-sm-12 col-m-8 col-l-6 col-xl-4">
                     <h1>Made friends with {this.state.matchCount} pups so far!</h1>
-                    <Card image={this.state.image} handleThumbsUp={this.handleThumbsUp} handleThumbDown={this.getNewPup} />
+                    <Card image={this.state.image} handleThumbsUp={this.handleThumbsUp} handleThumbsDown={this.getNewPup} />
                     <Alert style={{ opacity: this.state.match ? 1 : 0 }} type="success">Hooray! They Like You, Too!</Alert>
                     </div>
                 </div>
