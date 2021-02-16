647de8b833319632b036ef8b1a19be645ff318e0
JohnDJake <john.d.jake@gmail.com>
2021-02-16 13:29:42 -0800

    display random image

--- src/components/pages/Discover/index.js
+++ src/components/pages/Discover/index.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import API from '../../../utils/API';
 
 export default class Discover extends Component {
     render() {
         return (
             <div>
-                
+                <h1 className="text-center">Make New Friends</h1>
+                <h3 className="text-center">Thumbs up on any pups you'd like to meet!</h3>
+                <div className="card">
+                    <img src={API.random()} className="card-img" />
+                </div>
             </div>
         )
     }
