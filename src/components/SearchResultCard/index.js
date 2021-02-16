1d55665e073d357746bc8f4c95264531f37f9819
HannahStarcevich <starcevichh@gmail.com>
2021-02-16 14:49:31 -0800

    updates to cards

--- src/components/SearchResultCard/index.js
+++ src/components/SearchResultCard/index.js
@@ -2,17 +2,15 @@ import React from "react";
 import "./style.css";
 
 function SearchResultCard(props) {
-    const list = props.results || [];
+
   return (
-    list.map(results => (
         <div className="card">
         <div className="img-container">
-          <img alt="dog photo" className="img-fluid" src={results}  />
+          <img alt="dog photo" className="img-fluid" src={props.result}  />
         </div>
       </div>
 
-    ))
-  );
-}
+    )
+  }
 
 export default SearchResultCard;
