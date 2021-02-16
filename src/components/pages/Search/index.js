c391e167eb1e99cbf15cb161f47352bb03d5d1bb
eubank87 <eubank.ashley@gmail.com>
2021-02-16 13:28:00 -0800

    started search page

--- src/components/pages/Search/index.js
+++ src/components/pages/Search/index.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
+// import API from "../utils/API"
 
 export default class Search extends Component {
+    state = {
+        search: "",
+        results: [],
+        breeds: []
+    }
+
+    componentDidMount(){
+        API.search("labrador")
+        .then(res=> this.setState({breeds: res.data.message}))
+        .catch(err=> console.log(err))
+    }
+
     render() {
         return (
             <div>
